1d310da383e158215ff56d84cfa27715e3dfa764
Akshay Niranjan <anstmfriend@gmail.com>
2020-07-22 09:32:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function iterativeLog(arr){
   arr.forEach((elem,ind) => {console.log(`${index}: ${element}`)})
 }
 function iterate(callback){
-  arr=new Array()
+  arr=["new Array()"]
+  arr.forEach(callback)
 }
